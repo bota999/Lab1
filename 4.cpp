fe445689b61e4e7dbecd1f13b6ecb45092f9e0b1
bota999 <botakan0990@gmail.com>
2016-09-21 22:56:24 +0600

    Add files via upload

---
+++ 4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout<<"1 Maratov Marat Maratovich"<<endl;
+	cout<<"2 Armanov Arman Armanovih"<<endl;
+	cout<<"3 Kairatov Kairat Kairatovich"<<endl;
+
+ return 0;
+}
+
+
+
---
+++ 5.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+          double a,b,c;
+	cin>>a>>b;
+	c=sqrt(a*a+b*b);
+	cout<<c;
+
+
+ return 0;
+}
+
+
+
+
+
+
